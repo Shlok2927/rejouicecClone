80d534c0ccb02cbfd229c56c8f953568e15b8aef
Shlok2729 <shlokgupta0727@gmail.com>
2023-12-14 20:37:27 +0530

    the video has been started successfully

--- app/Components/Firstpage/FirstPage.js
+++ app/Components/Firstpage/FirstPage.js
@@ -1,7 +1,17 @@
+"use client";
 import React from "react";
-
+import "./FirstPage.css"; // Use a relative path
 const FirstPage = () => {
-  return <></>;
+  return (
+    <>
+      <section id="first_page">
+        hehe
+        <video width="100vw" height="100vh" autoPlay muted loop>
+          <source src="/demo.mp4" type="video/mp4" />
+        </video>
+      </section>
+    </>
+  );
 };
 
 export default FirstPage;
--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 "use client";
 import React from "react";
+import FirstPage from "./Components/Firstpage/FirstPage";
 
 const page = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      below down is the first page created
+      <FirstPage />
+    </>
+  );
 };
 
 export default page;
