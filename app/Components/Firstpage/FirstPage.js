d543585696a0f9309e73f43c04362412e3630f1e
Shlok2729 <shlokgupta0727@gmail.com>
2023-12-15 10:32:03 +0530

    first page has been created

--- app/Components/Firstpage/FirstPage.js
+++ app/Components/Firstpage/FirstPage.js
@@ -1,15 +1,43 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
+import gsap from "gsap";
+// import ScrollTrigger from "gsap-trial/ScrollTrigger";
 import "./FirstPage.css"; // Use a relative path
 const FirstPage = () => {
-  let mover = document.querySelector("#firstCursor");
+  // basic approach will be configured using normal js
+  // let followMe = (dets) => {
+  //   let mover = document.querySelector("#firstCursor");
+  //   mover.style.left = dets.clientX + "px";
+  //   mover.style.top = dets.clientY + "px";
+  // };
   let followMe = (dets) => {
-    console.log("Hi I am moving");
-    console.log(dets.clientX);
-    mover.style.border = "2px solid red";
+    const containerWidth = window.innerWidth;
+    const containerHeight = window.innerHeight;
+    let cursorX = dets.clientX;
+    let cursorY = dets.clientY;
+    const centerX = innerWidth / 2;
+    const centerY = innerHeight / 2;
+
+    if (cursorX < centerX) {
+      cursorX = cursorX - (centerX - cursorX) / 2;
+    } else if (cursorX > centerX) {
+      cursorX = cursorX + (cursorX - centerX) / 2;
+    }
+
+    if (cursorY < centerY) {
+      cursorY = cursorY - (centerY - cursorY) / 2;
+    } else if (cursorY > centerY) {
+      cursorY = cursorY + (cursorY - centerY) / 2;
+    }
+
+    gsap.to("#firstCursor", {
+      x: cursorX,
+      y: cursorY,
+    });
   };
   return (
     <>
+      {/* <section id="first_page" onMouseMove={followMe}> */}
       <section id="first_page" onMouseMove={followMe}>
         <div id="firstCursor">Play Reel</div>
         <nav id="firstNavigation">
